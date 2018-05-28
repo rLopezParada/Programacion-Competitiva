d400216f2518a13ed7738193eaf77370a194147e
rLopezParada <37628283+rLopezParada@users.noreply.github.com>
2018-05-28 16:35:27 -0400

    Add files via upload

--- enProgreso/10258.c
+++ enProgreso/10258.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 
 typedef struct {
-	int n,time,playing,score;
-	int problem[9];
-	int problemPenalties[9];
-}team;
+	int n,
+		time,
+		playing,
+		score,
+		problem[9],
+		problemPenalties[9];
+} team;
 
 int cmp(const void *a, const void *b){
 	int r = (*(team*)b).score-(*(team*)a).score;
@@ -18,28 +21,51 @@ int cmp(const void *a, const void *b){
 	return r;
 }
 
-/*int cmp(const void *a, const void *b){
-	int r = (int)(*(char*)a-*(char*)b);
-	return r;
-}*/
+void swap(team* team1,team* team2){
+	int aux;
+	aux=(*team2).n;
+	(*team2).n=(*team1).n;
+	(*team1).n=aux;
+
+	aux=(*team2).time;
+	(*team2).time=(*team1).time;
+	(*team1).time=aux;
+
+	aux=(*team2).playing;
+	(*team2).playing=(*team1).playing;
+	(*team1).playing=aux;
+
+	aux=(*team2).score;
+	(*team2).score=(*team1).score;
+	(*team1).score=aux;
+
+	int i;
+	for(i=0;i<9;i++){
+		aux=(*team2).problem[i];
+		(*team2).problem[i]=(*team1).problem[i];
+		(*team1).problem[i]=aux;
+
+		aux=(*team2).problemPenalties[i];
+		(*team2).problemPenalties[i]=(*team1).problemPenalties[i];
+		(*team1).problemPenalties[i]=aux;
+	}
+	return;
+}
 
 int main(){
-	int ncases,n,i,j;
-	int contestant, problem, time;
-	int ch;
-	int size;
-	int last;
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+
+	int ncases,n,i,j,contestant,problem,time,ch,size,last;
 	team aux;
 	char input[14];
-	team* teams=(team*)malloc(100*sizeof(team));	//new objects
-	for(i=0;i<20;i++){
-		printf("%ld\n",&teams[i]);
-	}
+	team* teams;
 	scanf("%d",&ncases);
 	getchar();
 
 	for(n=0;n<ncases;n++){
-		for(i=0;i<100;i++){	//constructor
+		teams=(team*)malloc(100*sizeof(team));	/*new objects*/
+		for(i=0;i<100;i++){	/*constructor*/
 			teams[i].n=i+1;
 			teams[i].time=teams[i].playing=teams[i].score=0;
 			for(j=0;j<9;j++){
@@ -55,7 +81,6 @@ int main(){
 			getchar();
 
 			sscanf(input,"%d %d %d %c",&contestant,&problem,&time,&ch);
-			//printf("%d %d %d %c\n",contestant,problem,time,ch);
 			contestant--;
 			problem--;
 			teams[contestant].playing=1;
@@ -69,22 +94,39 @@ int main(){
 				}
 			}
 		}
-		size=0;
-		last=0;
+		size=last=0;
 		for(i=0;i<100;i++){
 			if(teams[i].playing==1){
-				aux=teams[last];
-				teams[last]=teams[i];
-				teams[i]=aux;
 				size++;
-				last=i+1;
 			}
+		}
+		while(last<100){
+			for(i=last;i<100;i++){
+				if(teams[i].playing==0){
+					last=i;
+					break;
+				}
+			}
+			for(i=last;i<100;i++){
+				if(teams[i].playing==1){
+					swap(&teams[i],&teams[last]);
+					/*aux.n=aux.time=aux.playing=aux.score=0;
+					aux=teams[last];
+					teams[last]=teams[i];
+					teams[i]=aux;*/
+					break;
+				}
+			}
+			last++;
 		}
 		qsort(teams,size,sizeof(team),cmp);
 		for(i=0;i<size;i++){
 			printf("%d %d %d\n",teams[i].n,teams[i].score,teams[i].time);
 		}
+		free(teams);
+		if(n!=ncases-1){
+			printf("\n");
+		}
 	}
-	free(teams);
 	return 0;
 }
